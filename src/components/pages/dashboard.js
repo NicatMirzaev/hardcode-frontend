1d75a7e93ffc12f8b77ceb24e2d7cb242d4e71c2
NicatMirzoev <nicatmirzoev111@gmail.com>
2020-12-10 14:48:10 +0400

    added dropdown to dashboard

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -3,6 +3,24 @@ import Navbar from '../ui/dashboard/navbar';
 import Card from '../ui/card';
 
 const Dashboard = props => {
+  const [menu, setMenu] = React.useState(false);
+
+  React.useEffect(() => {
+
+    function handleClick(e){
+      if(menu === true && e.target.outerText !== "Account settings" && e.target.outerText !== "Support"){
+        setMenu(false);
+      }
+    }
+
+    window.addEventListener('mousedown', handleClick);
+
+    return () => {
+      window.removeEventListener('mousedown', handleClick);
+    }
+
+  }, [menu])
+
   if(props.user.isLoading === true) {
     return (
       <div className="w-full h-full flex justify-center">
@@ -19,7 +37,21 @@ const Dashboard = props => {
   return (
     <div className="flex w-full h-full">
       <div className="flex sm:w-1/5 w-1/3">
-        <Navbar/>
+      {menu === true &&
+        <div className="origin-top-left fixed left-0 mt-2 w-56 ml-20 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            <p className="block mb-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Seviye 4 (XP 2500 / 5000)</p>
+            <div style={{width: '100%', backgroundColor: '#ddd', height: '10px'}}>
+              <div style={{width: '50%', backgroundColor: '#4CAF50', height: '10px'}}/>
+            </div>
+            <a href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Profil</a>
+            <button className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900" role="menuitem">
+                Çıkış Yap
+            </button>
+          </div>
+        </div>
+      }
+        <Navbar menu={menu} setMenu={setMenu}/>
       </div>
       <div className="flex w-full justify-right flex-wrap">
         <div className="flex my-12 md:w-full w-3/4">
--- src/components/ui/dashboard/navbar.js
+++ src/components/ui/dashboard/navbar.js
@@ -4,23 +4,12 @@ import HomeIcon from '../../../icons/home.svg';
 import SearchIcon from '../../../icons/search.svg';
 import LeaderboardIcon from '../../../icons/leaderboard.svg';
 
-const dropdownStyle = {
-  display: "inline-block",
-  width: 0,
-  height: 0,
-  marginLeft: ".255em",
-  verticalAlign: ".255em",
-  content: "",
-  borderTop: ".3em solid",
-  borderRight: ".3em solid transparent",
-  borderBottom: 0,
-  borderLeft: ".3em solid transparent"
-}
+
 const Navbar = props => {
   return (
     <div style={{backgroundColor: '#323f4b'}} className="flex flex-col fixed items-center w-16 h-full">
       <div className="flex mt-3 mb-16">
-        <img src="https://i.imgur.com/8Km9tLL.jpg" className="rounded-full border-solid border-white border-2 w-12 cursor-pointer"/>
+        <img onClick={() => props.setMenu(!props.menu)} src="https://i.imgur.com/8Km9tLL.jpg" className="rounded-full border-solid border-white border-2 w-12 cursor-pointer"/>
       </div>
       <div className="flex flex-col mb-12">
         <img className="mb-8 cursor-pointer" src={HomeIcon}/>
