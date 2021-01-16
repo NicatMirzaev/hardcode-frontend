4aeebf87c017d3d12bdbe5bba6156175ef858f0e
NicatMirzoev <nicatmirzoev111@gmail.com>
2021-01-16 15:26:04 +0400

    deploy

--- src/components/pages/discover.js
+++ src/components/pages/discover.js
@@ -221,7 +221,7 @@ const Discover = props => {
       <div className="flex flex-col mt-6 w-full">
         <h2 className="text-2xl tracking-tight mx-auto mb-6 leading-10 font-extrabold text-gray-900 sm:text-3xl sm:leading-none md:text-4xl">HardCode'u Keşfet!</h2>
         <div className="sm:w-1/2 w-full pl-6 pr-3 mb-6 mx-auto">
-          <span style={{top: "90px"}} className="absolute text-gray-700 flex items-center pl-2">
+          <span className="search-discover absolute text-gray-700 flex items-center pl-2">
             <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
               <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </button>
